a88eac09ae5fcaca5ee2ebb5e50e87bca9e43c91
Alexis Yushin <alexis@apexive.com>
2023-10-04 14:46:15 -0400

    initial commit

---
+++ df_geolocation/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class DfGeolocationConfig(AppConfig):
+    name = "df_geolocation"
+    verbose_name = _("Django DF GEO Location")
+
+    class DFMeta:
+        api_path = "df_geolocation/"
