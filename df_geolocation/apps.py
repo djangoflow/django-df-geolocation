4616ef040b8593f74f9f5cf3329992370c8cc303
Alexis Yushin <alexis@apexive.com>
2023-10-05 11:07:07 -0400

    cleanup

--- df_geolocation/apps.py
+++ df_geolocation/apps.py
@@ -5,6 +5,3 @@ from django.utils.translation import gettext_lazy as _
 class DfGeolocationConfig(AppConfig):
     name = "df_geolocation"
     verbose_name = _("Django DF GEO Location")
-
-    class DFMeta:
-        api_path = "geolocation/"
